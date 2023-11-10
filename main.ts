7eb58e72c026854252775255db7cebb0bacb8147
Jacob Hummer <jcbhmr@outlook.com>
2023-11-10 12:51:45 -0600

    import assert

--- main.ts
+++ main.ts
@@ -13,6 +13,7 @@ import * as core from "@actions/core";
 import * as tc from "@actions/tool-cache";
 import * as YAML from "yaml";
 import { $ } from "execa";
+import assert from "node:assert/strict";
 
 const mainTemplate = (fileRelativePath: string, localBunVersion: string) => `\
 const fileRelativePath = ${JSON.stringify(fileRelativePath)};
