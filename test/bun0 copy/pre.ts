4ea3f016e2e496453dbf1deede26abccff15ffde
Jacob Hummer <jcbhmr@outlook.com>
2023-11-09 20:48:13 -0600

    bun 1

--- test/bun0 copy/pre.ts
+++
@@ -1,6 +0,0 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
-
-console.log("pre");
-console.log(`Hello ${core.getInput("name")}!`);
-core.setOutput("time", new Date().toLocaleTimeString());
