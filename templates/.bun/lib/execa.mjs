6858dc7b181011bac5b56a21fd5f026170be0622
jcbhmr <jcbhmr@outlook.com>
2023-11-11 13:04:43 -0600

    fix

--- templates/.bun/lib/execa.mjs
+++ templates/.bun/lib/execa.mjs
@@ -4,16 +4,16 @@ import { once } from "node:events";
 
 export function $(strings, ...values) {
   if (!Array.isArray(strings)) {
-    return $.bind(Object.assign(this, strings));
+    return $.bind(Object.assign(this ?? {}, strings));
   }
-  const keyFor = (index) => `__VALUES[${index}]__`;
-  const keyRe = /__VALUES\[(\d+)\]__/g;
+  const keyFor = (index) => `__values[${index}]`;
+  const keyRe = /__values\[(\d+)\]/g;
   const cmdWithKeys = strings
     .flatMap((s, i) => (i ? [keyFor(i - 1), s] : s))
     .join("");
   let argv = cmdWithKeys.split(/\s+/);
   argv = argv.map((arg) => arg.replace(keyRe, (m, i) => values[i]));
-  const argv0 = argv.unshift();
+  const argv0 = argv.shift();
   const subprocess = spawn(argv0, argv, this);
   const p = once(subprocess, "exit");
   subprocess.then = p.then.bind(p);
