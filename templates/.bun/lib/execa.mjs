e3bd912812ed3e37e74020038a6792fb4c95dcc7
Jacob Hummer <jcbhmr@outlook.com>
2023-11-12 12:19:22 -0600

    no infinite async recursion

--- templates/.bun/lib/execa.mjs
+++ templates/.bun/lib/execa.mjs
@@ -2,16 +2,6 @@
 import { spawn } from "node:child_process";
 import { once } from "node:events";
 
-async function promisifyChildProcess(cp, options = {}) {
-  const { reject = true } = options
-  const [exitCode] = await once(cp, "exit");
-  if (reject && exitCode) {
-    throw cp;
-  } else {
-    return cp;
-  }
-}
-
 export function $(strings, ...values) {
   if (!Array.isArray(strings)) {
     return $.bind(Object.assign(this ?? {}, strings));
@@ -25,9 +15,13 @@ export function $(strings, ...values) {
   argv = argv.map((arg) => arg.replace(keyRe, (m, i) => values[i]));
   const argv0 = argv.shift();
   const cp = spawn(argv0, argv, this);
-  const p = promisifyChildProcess(cp, this ?? {});
-  cp.then = p.then.bind(p);
-  cp.catch = p.catch.bind(p);
-  cp.finally = p.finally.bind(p);
+  cp.then = once(cp, "exit").then(([exitCode]) => {
+    delete cp.then
+    if ((this?.reject ?? true) && exitCode) {
+      throw cp;
+    } else {
+      return cp;
+    }
+  });
   return cp;
 }
